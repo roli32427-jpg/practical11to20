a79bb3f117ad1857014ec4b8417a7e1c0d200b6f
roli32427-jpg <roli32427@gmail.com>
2025-09-10 22:57:47 +0530

    practical11to37

--- practical37.php
+++ practical37.php
@@ -3,12 +3,13 @@
 <body>
 <form method="GET" action="">
   Enter your name: <input type="text" name="name">
+  Enter your age: <input type="number" name="age">
   <input type="submit" value="Submit">
 </form>
 
 <?php
 if (isset($_GET['name'])) {
-    echo "Hello, " . $_GET['name'];
+    echo "Hello, " . $_GET['name'].",you are" .$_GET['age']. "year old";
 }
 ?>
 </body>
